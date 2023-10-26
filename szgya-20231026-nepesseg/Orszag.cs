5ea642c033930abd1b030a63f04599579e371739
Airamek <vegkeppnemairamek@gmail.com>
2023-10-26 08:34:14 +0200

    Add project files.

---
+++ szgya-20231026-nepesseg/Orszag.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szgya_20231026_nepesseg
+{
+    class Orszag
+    {
+        public string Orszagnev { get; private set; }
+        public int Terulet { get; private set; }
+        public int Nepesseg { get; private set; }
+        public string Fovaros { get; private set; }
+        public int FovarosNepesseg { get; private set; }
+
+        public int Nepsuruseg => this.Nepesseg / this.Terulet;
+
+        public bool MeghaladjaA30Szazalekot => this.FovarosNepesseg > this.Nepesseg * 0.3;
+
+        public Orszag(string orszagnev, int terulet, int nepesseg, string fovaros, int fovarosNepesseg)
+        {
+            this.Orszagnev = orszagnev;
+            this.Terulet = terulet;
+            this.Nepesseg = nepesseg;
+            this.Fovaros = fovaros;
+            this.FovarosNepesseg = fovarosNepesseg;
+        }
+
+        public Orszag(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.Orszagnev = adatok[0];
+            this.Terulet = int.Parse(adatok[1]);
+            this.Nepesseg = int.Parse(adatok[2].Replace("g","0000"));
+            this.Fovaros = adatok[3];
+            this.FovarosNepesseg = int.Parse(adatok[4]) * 1000;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Orszagnev} ({this.Fovaros})";
+        }
+    }
+
+}
---
+++ szgya-20231026-nepesseg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace szgya_20231026_nepesseg
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            var orszagok = new List<Orszag>();
+
+            var sr = new StreamReader("../../../src/adatok-utf8.txt", Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                orszagok.Add(new Orszag(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"4.feladat\nA beolvasott országok száma {orszagok.Count}");
+
+            Console.WriteLine($"5.feladat\nKína népsűrűsége: {orszagok.Find(o => o.Orszagnev == "Kína").Nepsuruseg} fő/km^2");
+
+            Console.WriteLine($"6.feladat\nKínában a lakosság {orszagok.Find(o => o.Orszagnev == "Kína").Nepesseg - orszagok.Find(o => o.Orszagnev == "India").Nepesseg} fővel volt több.");
+
+            var harmadik = orszagok.OrderByDescending(o => o.Nepesseg).ToList()[2];
+            Console.WriteLine($"7.feladat\nA harmadik legnépesebb ország: {harmadik.Orszagnev}, a lakosság {harmadik.Nepesseg} fő.");
+
+            Console.WriteLine("8. feladat - A következő országok lakosságának több mint 30%-a a fővárosban lakik");
+            var fovarosbanTobbMintHarminc = orszagok.Where(o => o.MeghaladjaA30Szazalekot);
+            foreach (var orszag in fovarosbanTobbMintHarminc)
+            {
+                Console.WriteLine($"\t{orszag}");
+            }
+
+        }
+    }
+}
